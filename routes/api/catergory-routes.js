2bf892f31199379c347d278942253d9882292e3a
Mustafa Ali <mustafamire@gmail.com>
2021-10-03 19:40:50 +0100

    Add: post request to create new category

--- routes/api/catergory-routes.js
+++ routes/api/catergory-routes.js
@@ -39,6 +39,12 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
     // create a new category
+    try {
+        const categorytoCreate = await Category.create(req.body);
+        res.status(200).json(["A category is created as follows:", categorytoCreate]);
+    } catch (err) {
+        res.status(400).json(err);
+    }
 });
 
 router.put('/:id', (req, res) => {
